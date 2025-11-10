7bc0598f33e602b1c3f8d3aa02a88af960b5a75a
hai17 <hadjer.aziri@outscale.com>
2025-11-10 14:20:27 +0100

    Add

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ def hello():
     result = task.hello.delay(name)
     result.wait()
     return render_template('index.html', celery=result)
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=8080)
